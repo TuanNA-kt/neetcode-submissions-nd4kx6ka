2070c29be2cf0275e35fc2b8d14d2e93b8ab7048
Tuan Nguyen <100814095+TuanNA-kt@users.noreply.github.com>
2026-04-12 03:48:26 +0700

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,38 @@
+class TrieNodeDict:
+    def __init__(self):
+        self.word = False
+        self.child_map = {}
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNodeDict()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if (c not in cur.child_map):
+                cur.child_map[c] = TrieNodeDict()
+            cur = cur.child_map[c]
+        cur.word = True
+
+    def search(self, word: str) -> bool:
+        word_stack = [self.root]
+        
+        for c in word:
+            new_word_stack = []
+            while (word_stack):
+                word_node = word_stack.pop()
+                if (c ==  '.'):
+                    for child_node in word_node.child_map.values():
+                        new_word_stack.append(child_node)
+                elif (c in word_node.child_map):
+                    new_word_stack.append(word_node.child_map[c])
+            if (not new_word_stack):
+                return False
+            else: word_stack = new_word_stack
+
+        for word in word_stack:
+            if(word.word): return True
+        return False
+
